40c38aa07339aa0a1452dd37b4d525ab44f0c873
sanketmaru <sanketmaru@gmail.com>
2015-07-23 09:12:06 +0530

    product model changes

--- js/components/dashboard/dashboard-edit-controller.js
+++ js/components/dashboard/dashboard-edit-controller.js
@@ -2,14 +2,16 @@ angular.module('sgDashboard')
 	.controller('DashboardEditCtrl', ['$scope',
 		function($scope){
 
+		$scope.product = {};
+
 		$scope.addProduct = function(){
-			$scope.product = "";
-			ProductService.addProduct(product)
+			console.log($scope.product);
+			/*ProductService.addProduct(product)
 				.then(function(result){
 					alert("Success !! Product added");
 				}, function(err){
 					alert(err);
-				});
+				});*/
 		};
 
 
