f55f2a31e5ffa45b8ba96bb084df3967164d15b5
Sanket Maru <sanket_maru@persistent.co.in>
2016-09-15 16:01:46 +0530

    Revert "initial commit for require"
    This reverts commit 7a8d1aa4458a7152971c00c73d1f6cac315a3cfd.

--- js/app.js
+++ js/app.js
@@ -1,26 +1,23 @@
 'use strict';
+angular.module('sgDashboard', [
+	'ui.router',
+	'sgProduct',
+	'sgMessage',
+	]).config(['$urlRouterProvider','$stateProvider',
+  function($urlRouterProvider, $stateProvider) {
 
-define(['angularAMD', 'angular-ui-route'], function (angularAMD) {
-	var app = angular.module('sgDashboard', ['ui.router','sgProduct','sgMessage']);
-	app.config(['$urlRouterProvider','$stateProvider',
-	  function($urlRouterProvider, $stateProvider) {
+    $urlRouterProvider.otherwise("/home");
 
-	    $urlRouterProvider.otherwise("/home");
-
-		  $stateProvider
-		    .state('home', {
-		      url: "/home",
-		      templateUrl: "templates/home.html",
-		      controller: "DashboardCtrl"
-		    })
-		    .state('edit', {
-		      url: "/edit",
-		      templateUrl: "templates/edit.html",
-		      controller: "DashboardEditCtrl"
-		    });
-	  }
-	]);
-
-	return angularAMD.bootstrap(app);
-
-});
+	  $stateProvider
+	    .state('home', {
+	      url: "/home",
+	      templateUrl: "templates/home.html",
+	      controller: "DashboardCtrl"
+	    })
+	    .state('edit', {
+	      url: "/edit",
+	      templateUrl: "templates/edit.html",
+	      controller: "DashboardEditCtrl"
+	    });
+  }
+]);
